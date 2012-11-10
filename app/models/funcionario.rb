826ced76c1354355ac369b0456dc4e417dc82ba5
Lucas Carvalho <dev@lucascarvalho.com.br>
2012-11-10 16:13:51 -0200

    Departamentos

---
+++ app/models/funcionario.rb
@@ -0,0 +1,6 @@
+class Funcionario < ActiveRecord::Base
+  attr_accessible :admissao, :departamento_id, :nome
+
+  belongs_to :departamento
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curso::Application.config.secret_token = '057671f68428db70cf5f4642a1869aeaf96c4ab6f5711d36cacf91962ffe851024db87cfb65e6e608c920a9e39de3838086cce635ee4e48241e67ff84a51df48'
