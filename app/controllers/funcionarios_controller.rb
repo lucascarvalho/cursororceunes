deddfd651b2575bded4be07346a662de3f200498
Lucas Carvalho <dev@lucascarvalho.com.br>
2012-11-11 10:15:13 -0200

    Formatação do campo data na atualização de funcionário

--- app/controllers/funcionarios_controller.rb
+++ app/controllers/funcionarios_controller.rb
@@ -18,11 +18,22 @@ class FuncionariosController < ApplicationController
 	def update
 	    @funcionario = Funcionario.find(params[:id])
 
+	    date = params[:funcionario][:admissao]
+	    @funcionario.admissao = text_to_date(date)
+
 		if @funcionario.update_attributes(params[:funcionario])
 			redirect_to @funcionario, notice: 'Funcionário atualizado com sucesso.'
 		else
 			render action: "edit"
 	    end
 	end
+
+
+	private
+		
+		def text_to_date(text)
+			parts = text.split("/")
+			Date.new parts[2].to_i, parts[1].to_i, parts[0].to_i
+		end
 
 end
